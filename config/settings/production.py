1707cfa36587d7b9da7cf9482fcc90f2d9a79c50
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2019-05-30 02:00:40 -0500

    Hi

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,6 +5,11 @@ from .base import *
 
 # DEBUG
 # -----------------------------------------------------------------------------
+
+SECRET_KEY = env(
+	"DJANGO_SECRET_KEY",
+	default="pkKvvaquTxQfVyBFlSIwht5sj2wZy4M4vocucuvrMaTYpdAOedHYpheN9JKzSzHv",
+)
 DEBUG = False
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 # This ensures that Django will be able to detect a secure connection
